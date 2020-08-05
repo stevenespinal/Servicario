9a91bcdd04db0b27b74006d5e5fcf4e47c02321e
Steven Espinal <stevenjesusespinal@gmail.com>
2020-08-05 13:28:20 -0400

    middleware improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,36 +10,36 @@ const Navbar = ({id}) => {
             <div className="title">Servicio</div>
           </Link>
 
-          <a href="/" className="navbar-item is-hidden-desktop is-hidden-tablet">
+          <Link to="/" className="navbar-item is-hidden-desktop is-hidden-tablet">
             <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'visible'}}>
               <svg width="1000px" height="1000px">
-                <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
-                <path className="path2" d="M 300 500 L 700 500"></path>
-                <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+                <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"/>
+                <path className="path2" d="M 300 500 L 700 500"/>
+                <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"/>
               </svg>
-              <button id="menu-icon-trigger" className="menu-icon-trigger"></button>
+              <button id="menu-icon-trigger" className="menu-icon-trigger"/>
             </div>
-          </a>
+          </Link>
 
-          <a href="/" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+          <Link to="/" role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+          </Link>
         </div>
 
         <div id="navbar-menu" className="navbar-menu is-static">
           <div className="navbar-start">
-            <a href="/" className="navbar-item is-hidden-mobile">
+            <Link to="/" className="navbar-item is-hidden-mobile">
               <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'visible'}}>
                 <svg width="1000px" height="1000px">
-                  <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
-                  <path className="path2" d="M 300 500 L 700 500"></path>
-                  <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+                  <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"/>
+                  <path className="path2" d="M 300 500 L 700 500"/>
+                  <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"/>
                 </svg>
-                <button id="menu-icon-trigger" className="menu-icon-trigger"></button>
+                <button id="menu-icon-trigger" className="menu-icon-trigger"/>
               </div>
-            </a>
+            </Link>
           </div>
 
           <div className="navbar-end">
--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,46 @@
 import {combineReducers, createStore} from "redux";
 import servicesReducer from "../reducers";
 
-const addLoggerToDispatch = store => {
-  const initDispatch = store.dispatch;
-
-  return action => {
-    console.group(action.type);
-    console.log(`%c prev state`, `color: red`, store.getState());
-    console.log(`%c action`, `color: green`, action);
-    const returnValue = initDispatch(action);
-    console.log(`%c next state`, `color: orange`, store.getState());
-    // console.log(returnValue);
-    console.groupEnd(action.type);
-    return returnValue;
-  }
+const addLoggerToDispatch = store => nextDispatch => action => {
+  console.group(action.type);
+  console.log(`%c prev state`, `color: red`, store.getState());
+  console.log(`%c action`, `color: green`, action);
+  const returnValue = nextDispatch(action);
+  console.log(`%c next state`, `color: orange`, store.getState());
+  // console.log(returnValue);
+  console.groupEnd(action.type);
+  return returnValue;
 }
 
-const addPromiseToDispatch = store => {
-  const initDispatch = store.dispatch;
-  return action => {
-    if (typeof action.then === 'function') {
-      return action.then(initDispatch);
-    }
 
-    return initDispatch(action);
+const addPromiseToDispatch = store => nextDispatch => action => {
+  if (typeof action.then === 'function') {
+    return action.then(nextDispatch);
   }
+  return nextDispatch(action);
+}
+
+
+const applyMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch);
+  });
 }
 
 const initStore = () => {
+  const middlewares = [addPromiseToDispatch];
   const serviceApp = combineReducers({
     service: servicesReducer
   });
 
   const browserSupport = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
   const store = createStore(serviceApp, browserSupport);
-  if (process.env.NODE_ENV !== 'production') store.dispatch = addLoggerToDispatch(store);
-  store.dispatch = addPromiseToDispatch(store)
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(addLoggerToDispatch);
+  }
+
+  applyMiddlewares(store, middlewares);
+
   return store;
 }
 
