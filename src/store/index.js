7bb1cbdc63f980d776752ebe3715720453ec50e2
Steven Espinal <stevenjesusespinal@gmail.com>
2020-08-08 15:50:31 -0400

    cleaned up store by using enhancers from redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,15 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import serviceApp from "../reducers";
-
-const logger = store => nextDispatch => action => {
-  console.group(action.type);
-  console.log(`%c prev state`, `color: red`, store.getState());
-  console.log(`%c action`, `color: green`, action);
-  const returnValue = nextDispatch(action);
-  console.log(`%c next state`, `color: orange`, store.getState());
-  // console.log(returnValue);
-  console.groupEnd(action.type);
-  return returnValue;
-}
-
-const thunk = store => nextDispatch => action => {
-  if (typeof action === 'function') {
-    return action(store.dispatch, store.getState);
-  } else {
-    return nextDispatch(action);
-  }
-}
-
-const applyMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch);
-  });
-}
+import thunk from "redux-thunk";
+import {createLogger} from "redux-logger";
 
 const initStore = () => {
   const middlewares = [thunk];
-  const browserSupport = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-  const store = createStore(serviceApp, browserSupport);
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
-
-  applyMiddlewares(store, middlewares);
-
-  return store;
+  return createStore(serviceApp, composeEnhancers(applyMiddleware(...middlewares)));
 }
 
 export default initStore;
