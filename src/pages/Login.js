c860bec69741ac5a30116878a05b302cddc67cbc
Steven Espinal <stevenjesusespinal@gmail.com>
2020-08-10 01:20:19 -0400

    added react-hook-form to store form data

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
           <p className="subtitle has-text-grey">Please login to proceed.</p>
           <div className="box">
             <figure className="avatar">
-              <img src="https://placehold.it/128x128"/>
+              <img src="https://placehold.it/128x128" alt="placeholder"/>
             </figure>
             <form>
               <div className="field">
@@ -43,9 +43,9 @@ const Login = () => {
             </form>
           </div>
           <p className="has-text-grey">
-            <a>Sign In With Google</a>&nbsp;
+            <a href="/">Sign In With Google</a>&nbsp;
             <a href="/">Sign Up</a> &nbsp;·&nbsp;
-            <a href="../">Need Help?</a>
+            <a href="/">Need Help?</a>
           </p>
         </div>
       </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RegisterForm from "../components/auth";
 
 const Register = () => {
 
@@ -10,82 +11,14 @@ const Register = () => {
           <p className="subtitle has-text-grey">Please Register to proceed.</p>
           <div className="box">
             <figure className="avatar">
-              <img src="https://placehold.it/128x128" />
+              <img src="https://placehold.it/128x128" alt="placeholder"/>
             </figure>
-            <form>
-              <div className="field">
-                <div className="control">
-                  <input name="email"
-                         className="input is-large"
-                         type="email"
-                         placeholder="Your Email"
-                         autofocus=""
-                         autocomplete="email" />
-                  <div className="form-error">
-                    <span className="help is-danger">Email is required</span>
-                    <span className="help is-danger">Email address is not valid</span>
-                  </div>
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input name="fullName"
-                         className="input is-large"
-                         type="text"
-                         placeholder="Full Name"
-                         autofocus=""/>
-                  <div className="form-error">
-                    <span className="help is-danger">Name is required</span>
-                    <span className="help is-danger">Name is not valid</span>
-                  </div>
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input name="avatar"
-                         className="input is-large"
-                         type="text"
-                         placeholder="Avatar"
-                         autofocus=""/>
-                  <div className="form-error">
-                    <span className="help is-danger">Avatar is required</span>
-                    <span className="help is-danger">Avatart is not valid</span>
-                  </div>
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input name="password"
-                         className="input is-large"
-                         type="password"
-                         placeholder="Your Password"
-                         autocomplete="current-password" />
-                  <div className="form-error">
-                    <span className="help is-danger">Password is required</span>
-                  </div>
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input name="passwordConfirmation"
-                         className="input is-large"
-                         type="password"
-                         placeholder="Repeat Password"
-                         autocomplete="current-password" />
-                  <div className="form-error">
-                    <span className="help is-danger">Password Confirmation is required</span>
-                  </div>
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="button is-block is-info is-large is-fullwidth">Register</button>
-            </form>
+            <RegisterForm/>
           </div>
           <p className="has-text-grey">
-            <a>Sign In With Google</a>&nbsp;
+            <a href="/">Sign In With Google</a>&nbsp;
             <a href="/">Sign Up</a> &nbsp;·&nbsp;
-            <a href="../">Need Help?</a>
+            <a href="/">Need Help?</a>
           </p>
         </div>
       </div>
