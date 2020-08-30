310ee5d020cd5858f71e54194c439f16b58c005c
Steven Espinal <stevenjesusespinal@gmail.com>
2020-08-29 20:06:07 -0400

    Changed status to offline on logout, need to fix status in cloud firestore

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,16 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
 admin.initializeApp();
+const firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.addMessage = functions.https.onRequest(async (req, res) => {
- // Grab the text parameter.
- const original = req.query.text;
- const snapshot = await admin.database().ref('/messages').push({original: original});
- // Send back a message that we've succesfully written the message
- res.redirect(303, snapshot.ref.toString());
-
+exports.onUserStatusChanged = functions.database.ref('/status/{uid}').onUpdate(async (change, context) => {
+  const eventStatus = change.after.val();
+  const userFirestoreRef = firestore.doc(`/profiles/${context.params.uid}`);
+  const statusSnapshot = await change.after.ref.once('value');
+  const status = statusSnapshot.val();
+  if (status.last_changed > eventStatus.last_changed) {
+    return null;
+  }
+  eventStatus.last_changed = new Date(eventStatus.last_changed);
+  return userFirestoreRef.update(eventStatus);
 });
