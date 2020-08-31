5f9dbb46bf19889f8e4b6bf715a1cbad74be36e8
Steven Espinal <stevenjesusespinal@gmail.com>
2020-08-30 21:36:18 -0400

    Fixed firebase functions error

--- src/api/connection.js
+++ src/api/connection.js
@@ -3,7 +3,9 @@ import 'firebase/database';
 
 export const createFirebaseRef = (collection, id) => firebase.database().ref(`/${collection}/${id}`);
 
-export const onConnectionChanged = callback => firebase.database().ref('.info/connected').on('value', snapshot => callback(snapshot.val()));
+export const onConnectionChanged = callback => firebase.database().ref('.info/connected').on('value', snapshot => {
+  callback(snapshot.val())
+});
 
 export const isOffline = {
   state: 'offline',
