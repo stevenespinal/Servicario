094b7e04e40346b50a7dbbe45f76201743022e0d
Steven Espinal <stevenjesusespinal@gmail.com>
2020-09-12 21:54:58 -0400

    Fixed collaboration status in received collaboration page

--- src/pages/collaborations/ReceivedCollaborations.js
+++ src/pages/collaborations/ReceivedCollaborations.js
@@ -4,6 +4,7 @@ import withAuthorization from '../../components/hoc/withAuthorization'
 import {fetchCollaborations} from '../../actions';
 import moment from 'moment'
 import Spinner from "../../components/Spinner";
+import {Timestamp} from "../../db";
 
 class Collaborations extends Component {
 
@@ -17,19 +18,63 @@ class Collaborations extends Component {
     fetchCollaborations(user.uid).then(collaborations => this.setState({collaborations, loading: false}));
   }
 
-  statusClass = status => {
-    if (status === "pending") {
-      return 'is-warning';
-    } else if (status === "accepted") {
-      return 'is-success';
-    } else {
-      return 'is-danger';
+  getCollaborationStatusClass = expiresAt => {
+    if (!expiresAt) {
+      return {
+        className: 'is-danger',
+        value: 'Not Started'
+      };
     }
+
+    if (Timestamp.now().seconds < expiresAt.seconds) {
+      return {
+        className: 'is-warning',
+        value: 'In Progress'
+      };
+    } else {
+      return {
+        className: 'is-success',
+        value: 'Finished'
+      }
+    }
+  }
+
+  renderCollaborations = collaborations => {
+    return collaborations.map(c => {
+        const {className, value} = this.getCollaborationStatusClass(c.expiresAt)
+        return (
+          <article className="post" key={c.id}>
+            <h4>{c.title}</h4>
+            <div className="media">
+              <div className="media-left">
+                <p className="image is-32x32">
+                  <img src={c.image} alt={c.title}/>
+                </p>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <span>{c.fromUser.name}</span> replied {moment(c.createdAt.toDate()).fromNow()} &nbsp;
+                    <span className={`tag ${className}`}>{value}</span>
+                  </p>
+                </div>
+              </div>
+              <div className="media-right">
+            <span className="has-text-grey-light">
+              <Link to={`/collaborations/${c.id}`}>
+                <button className="button">Enter</button>
+              </Link>
+            </span>
+              </div>
+            </div>
+          </article>
+        )
+      }
+    )
   }
 
   render() {
     const {collaborations, loading} = this.state;
-
 
     if (loading) return <Spinner/>
     return (
@@ -37,34 +82,7 @@ class Collaborations extends Component {
         <div className="container">
           <h1 className="title">Collaborations</h1>
           <div className="box content">
-            {collaborations.map(c => (
-              <article className="post" key={c.id}>
-                <h4>{c.title}</h4>
-                <div className="media">
-                  <div className="media-left">
-                    <p className="image is-32x32">
-                      <img src={c.image} alt={c.title}/>
-                    </p>
-                  </div>
-                  <div className="media-content">
-                    <div className="content">
-                      <p>
-                        <span>{c.fromUser.name}</span> replied {moment(c.createdAt.toDate()).fromNow()} &nbsp;
-                        <span className={`tag ${this.statusClass(c.status)}`}>{c.status}</span>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="media-right">
-                    <span className="has-text-grey-light">
-                      <Link to={`/collaborations/${c.id}`}>
-                        <button className="button">Enter</button>
-                      </Link>
-                    </span>
-                  </div>
-                </div>
-              </article>
-            ))
-            }
+            {this.renderCollaborations(collaborations)}
           </div>
         </div>
       </div>
